7b701057dc6a7bb0c1d6365909a75bd7597bd66a
Thiago Costa <thiagohrcosta86@gmail.com>
2021-05-20 18:57:46 -0300

    Adds second test wiht rspec

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -5,5 +5,12 @@ RSpec.describe Admin, type: :model do
     admin = Admin.new(email: "admin@admin.com", password: "123456")
 
     expect(admin.email).to eql "admin@admin.com"
+    expect(admin).to be_valid
+  end
+
+  it "admin is not valid" do
+    admin = Admin.new(email: "admin@admin.com")
+
+    expect(admin).to_not be_valid
   end
 end
